d87b5e6c3d7e1967cce745f16b910fea9d400dd4
Dostonbek <dostonbek200gmail.com>
2025-01-30 17:41:07 +0500

    Wather-obhavo

---
+++ js/app.js
@@ -0,0 +1,48 @@
+const changeLocation = document.getElementById('change-location')
+const card = document.getElementById('card')
+const details = document.getElementById('details')
+const weatherIcon = document.getElementById('weather-icon')
+const overlay = document.getElementById('overlay')
+ changeLocation.city.focus()
+
+ function loader (state){
+  if(state){
+    overlay.classList.remove("d-none")
+  }else{
+    overlay.classList.add("d-none")
+  }
+ }
+
+const updateUI = (weather)=>{
+    console.log(weather)
+ details.innerHTML=`
+ <h5 class="mb-3">${weather.name},${weather.sys.country}</h5>
+          <p class="mb-3">${weather.weather[0].main}</p>
+          <div class="display-4 mb-3">
+            <span>${Math.round(weather.main.temp)}</span>
+            <span>&deg; C</span>
+          </div>
+ `
+ if(card.classList.contains("d-none")){
+    card.classList.remove("d-none")
+ }
+
+ weatherIcon.src = `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
+}
+
+// get wether
+const getWether = async(city)=>{
+const data = await getData(city)
+return data
+}
+
+
+// get location
+changeLocation.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const cityName =changeLocation.city.value.trim()
+    changeLocation.reset()
+    getWether(cityName).then((data)=>{
+        updateUI(data)
+    })
+})
